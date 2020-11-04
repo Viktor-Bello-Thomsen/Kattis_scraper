c5b73ce4cf9f05896d4d16316e4e2b0ada983a8d
Viktor Bello Thomsen <45241565+Viktor-Bello-Thomsen@users.noreply.github.com>
2020-11-04 15:16:44 +0100

    Code cleanup

--- scraper.py
+++ scraper.py
@@ -1,18 +1,14 @@
 from decouple import config
 from selenium import webdriver
 from selenium.webdriver.support.ui import WebDriverWait
-from selenium.webdriver.common.by import By
-
-email = config('EMAIL')
-password = config('PASSWORD')
-
-driver = webdriver.Chrome()
 
 def KattisLogin():
     driver.get("https://open.kattis.com/login/email?")
     driver.find_element_by_id("user_input").send_keys(email)
     driver.find_element_by_id("password_input").send_keys(password)
     driver.find_element_by_xpath("//input[@value='Submit']").click()
+    driver.implicitly_wait(5)
+    driver.get("https://open.kattis.com/problems?show_solved=on&show_tried=off&show_untried=off")
 
 def turnPage():
     try:
@@ -26,19 +22,23 @@ def handlePage():
     elements = driver.find_elements_by_xpath("/html/body/div[1]/div/div/section/div[2]/table/tbody/tr/td[1]/a")
     problems = []
     for i in elements:
-        #print(i, i.get_attribute("href"))
         problems.append(i.text)
     return problems
 
-KattisLogin()
-driver.get("https://open.kattis.com/problems?show_solved=on&show_tried=off&show_untried=off")
-handlePage()
-running = True
-things = []
-while(running):
-    things.extend(handlePage())
-    running = turnPage()
 
-with open("solved.txt", "w") as f:
-    for name in things:
-        f.write(name+"\n")
+if __name__ == "__main__":
+    email = config('EMAIL')
+    password = config('PASSWORD')
+    driver = webdriver.Chrome()
+
+    KattisLogin()
+    running = True
+    names = []
+    while(running):
+        names.extend(handlePage())
+        running = turnPage()
+
+    with open("solved.txt", "w") as f:
+        for name in names:
+            f.write(name+"\n")
+    driver.quit()
